5dbeb27c4b4791396af3a4236bda5ff0e17ff2ac
unknown <nsy63113@gmail.com>
2024-05-11 12:39:40 +0300

    updet1

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+    protected $table = 'users';
 
     /**
      * The attributes that should be hidden for serialization.
@@ -39,6 +40,7 @@ class User extends Authenticatable
      */
     protected function casts(): array
     {
+        
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
